16996acd940f0336c28fbb92d3af8e93987825d5
KDyulgerov <kdyulgerov.sport@gmail.com>
2025-08-03 10:00:06 +0300

    PROJ-222: Test failure confirmation

--- test/sum.test.js
+++ test/sum.test.js
@@ -1,1 +1,7 @@
-const sum = require('../sum'); QUnit.module('sum'); QUnit.test('sum two numbers should succeed', (assert) => {assert.equal(sum(1, 2), 3);});
+const sum = require('../sum');
+
+QUnit.module('sum');
+
+QUnit.test('sum two numbers should succeed', (assert) => {
+    assert.equal(sum(1, 2), 4);
+});
